d2338deb9374e745d9a6b1396c27b74231b65280
Matija Simin <marcussimon12082015@hotmail.com>
2019-12-08 09:47:16 -0800

    Added session controller with login logout actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,23 @@
 class SessionsController < ApplicationController
   get '/login' do
-
+    erb :'sessions/login'
   end
 
   post '/sessions' do
-
+    @user = User.find_by(email:params[:email]).try(:authenticate,params[:password])
+    if @user
+      session[:user_id] = @user.id
+      flash[:notice] = "Logged In! Welcome!"
+      redirect '/'
+    else
+      @message = "We can't log You in! Email or password incorrect!"
+      erb :'sessions/login'
+    end
   end
 
   get '/sessions/logout' do
-
+    session.clear
+    flash[:notice] = "Bye!"
+    redirect '/'
   end  
 end
