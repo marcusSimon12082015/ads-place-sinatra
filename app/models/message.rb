6882dd23141dfd7067fe773a1c5fbf5415ed31b9
Matija Simin <marcussimon12082015@hotmail.com>
2019-12-08 10:31:48 -0800

    added validations to message model for email and telephone number

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,6 +5,8 @@ class Message < ActiveRecord::Base
   validates :text, presence: true
   validates :sender_name, presence: true
   validates :telephone_number, presence: true
+  validates :telephone_number, format: {with: /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/}
   validates :email, presence: true
+  validates :email, format: { with: /\A[^@,\s]+@[^@,\s]+\.[^@,\s]+\z/}
   #####################################################
 end
