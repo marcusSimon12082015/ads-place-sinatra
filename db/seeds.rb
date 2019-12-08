cb415f3296e71fd6ae83d3cc5537b6b27af31dc5
Matija Simin <marcussimon12082015@hotmail.com>
2019-12-08 11:23:01 -0800

    added categories and conditions to seed file

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+#create conditions
+newCondition = Condition.create(condition_name:"New")
+likeNew = Condition.create(condition_name:"Like New")
+used = Condition.create(condition_name:"Used")
+
+#create categories
+books = Category.create(category_name:"Books")
+clothing = Category.create(category_name:"Clothing")
+boats = Category.create(category_name:"Boats")
+art = Category.create(category_name:"Art")
+antiques = Category.create(category_name:"Antiques")
+accessories = Category.create(category_name:"Accessories")
+costumes = Category.create(category_name:"Costumes")
+exercise = Category.create(category_name:"Exercise")
+bikes = Category.create(category_name:"Bikes")
