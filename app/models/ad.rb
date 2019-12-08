9b8e4bb8f17b040e934dd34455897c95c4d48b75
Matija Simin <marcussimon12082015@hotmail.com>
2019-12-08 10:36:05 -0800

    added validations to user and ad model

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -3,4 +3,13 @@ class Ad < ActiveRecord::Base
   belongs_to :category
   belongs_to :condition
   has_many :messages, dependent: :destroy
-end 
+
+  #validations
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :quantity, presence: true
+  validates :location, presence: true
+  validates :category, presence: true
+  validates :condition, presence: true 
+  #############################################################
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
   has_many :ads
 
+  #validations
+  validates :email, presence:true
+  validates :email, uniqueness: true
+  validates :email, format:{ with: /\A[^@,\s]+@[^@,\s]+\.[^@,\s]+\z/}
+  ###############################################
   has_secure_password
-end 
+end
