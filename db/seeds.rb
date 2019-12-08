6d7bc2304258e050de13eede59107f3206cac612
Matija Simin <marcussimon12082015@hotmail.com>
2019-12-08 11:29:32 -0800

    added users and ads part 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,37 @@ accessories = Category.create(category_name:"Accessories")
 costumes = Category.create(category_name:"Costumes")
 exercise = Category.create(category_name:"Exercise")
 bikes = Category.create(category_name:"Bikes")
+
+#create user
+marcus = User.create(email:"marcus@email.com",password:"11111111")
+jeff = User.create(email:"marcus@email.com",password:"22222222")
+greg = User.create(email:"marcus@email.com",password:"22222222")
+
+#create ads
+ad1 = Ad.create(
+  description:"Barely used bike!",
+  price:45.0,
+  quantity:1,
+  location:"Hawai",
+  condition:likeNew,category:bikes,user:marcus,
+  image:""
+)
+
+ad2 = Ad.create(
+  description:"Barely used boat!",
+  price:450.0,
+  quantity:1,
+  location:"New York",
+  condition:used,category:boats,user:marcus,
+  image:""
+)
+
+ad3 = Ad.create(
+  description:"Barely used bike!",
+  price:450.0,
+  quantity:1,
+  location:"Hawai",
+  condition:used,category:bikes,user:marcus,
+  image:""
+)
+marcus.ads << [ad1,ad2,ad3]
