7473840e114e03b0680aaf4c8d90a4b79ddbc20d
Matija Simin <marcussimon12082015@hotmail.com>
2019-12-12 09:01:00 -0800

    added delete post actions to message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,10 +5,31 @@ class MessageController < ApplicationController
   end
 
   post '/message' do
-
+    @message = Message.create(params[:message])
+    if @message.save
+      flash[:notice] = "Message was sent!"
+      @ad = Ad.find_by(id:params[:ad_id])
+      @ad.messages << @message
+      @message.ad = @ad
+    else
+      @ad_id = params[:ad_id]
+      erb :'message/new'
+    end 
   end
 
   delete '/message/:id' do
-
-  end   
+    begin
+      @ad = Ad.find_by!(id:params[:ad_id])
+      @message = @ad.messages.delete(params[:id])
+      if @message.count == 1 && @message[0].destroyed?
+        flash[:notice] = "Message is deleted!"
+      else
+        flash[:error] = "Message was not deleted!"
+      end
+    rescue ActiveRecord::RecordNotFound
+      flash[:error] = "Ad not found!"
+    ensure
+      redirect request.env['HTTP_REFERER']
+    end
+  end
 end
