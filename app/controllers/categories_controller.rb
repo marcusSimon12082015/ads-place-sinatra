9c623db7211df3dd90fa0e26543cf6e76693a3d1
Matija Simin <marcussimon12082015@hotmail.com>
2019-12-12 08:30:56 -0800

    Added show view for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoryController < ApplicationController
   get '/category/:id' do
-    
+    @category = Category.find_by_id(params[:id])
+    @ads = @category.ads
+    erb :"categories/show", :layout => :"home_category_layout"
   end
 end
