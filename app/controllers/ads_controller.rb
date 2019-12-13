7d861127b0d025864841a36463c487c8469b06ba
Matija Simin <marcussimon12082015@hotmail.com>
2019-12-13 09:23:57 -0800

    added new and edit actions to ads controller

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,4 +1,35 @@
 class AdsController < ApplicationController
+  #new
+  get '/ad/new' do
+    if logged_in?
+      @categories = Category.all
+      @conditions = Condition.all
+      @action = "new"
+      erb :'ads/new.html'
+    else
+      flash[:error] = "You are not allowed to perform this action!Login or Sign Up"
+      redirect to("/")
+    end
+  end
+
+  #edit
+  get '/ad/:id/edit' do
+    @ad = Ad.find_by(id:params[:id])
+    if @ad.nil?
+      flash[:error] = "Ad Not Found!"
+      redirect "/"
+    else
+      if adOwner?(@ad.user)
+        @categories = Category.all
+        @conditions = Condition.all
+        erb :'ads/edit.html'
+      else
+        flash[:error] = "You are not allowed to perform this action!"
+        redirect to("/")
+      end
+    end
+  end
+  #show
   get '/ad/:id' do
     @ad = Ad.find_by(id:params[:id])
     if @ad.nil?
@@ -7,4 +38,7 @@ class AdsController < ApplicationController
     end
     erb :"ads/show.html"
   end
+
+  #create
+  
 end
