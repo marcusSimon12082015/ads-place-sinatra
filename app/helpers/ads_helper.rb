6925e21fd9ab277c45995d51089059ac4156c0f0
Matija Simin <marcussimon12082015@hotmail.com>
2019-12-13 09:44:30 -0800

    added a question mark to a method

--- app/helpers/ads_helper.rb
+++ app/helpers/ads_helper.rb
@@ -2,7 +2,7 @@ module AdsHelper
   def displayAdsImage(ad)
     ad.image.file.nil? ? "/public/images/Poster_not_available.png" : ad.image.url.to_s
   end
-  def adOwner(user)
+  def adOwner?(user)
     !current_user.nil? && current_user.id == user.id
   end
 end
